021024389c81cf5939e38ab4ad823dd9e0f8b9bc
Crowdbotics <team@crowdbotics.com>
2020-05-06 05:12:41 +0000

    Update models

--- plat_v1_16655/wsgi.py
+++ plat_v1_16655/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plat_v1_16655.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plat_v1_16655.settings")
 
 application = get_wsgi_application()
